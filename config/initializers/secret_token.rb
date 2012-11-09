bfe6df77bc5da6c9b90c757b06f1092b56a80941
Wes Bangerter <wes@custombit.com>
2012-11-09 08:57:32 -0700

    Initial Rails 2.3.8 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scanner::Application.config.secret_token = '03267930448f1eb1c6a48fbe04d5d546837c70d3ac005266a169f3c84b120cd9d16e4fb051867bf99022f80e0f84e5df5ea7dd6ede65ce3f1ea33e90864cc2aa'
