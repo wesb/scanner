a34be2c05f09c21130feb478ebdf7ef719e10bb5
Wes Bangerter <wes@custombit.com>
2012-11-09 22:59:31 -0700

    Allow customer#purchase without loggin in

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,7 @@
 class CustomersController < ApplicationController
 
   load_and_authorize_resource
+  skip_load_resource only: [:purchase]
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to login_url
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :products, Customer
     return if user.nil?
     if user
       can :manage, :all
